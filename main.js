e2b448413c5b326ddd464d0a66d6cd004a4250ec
fatimafarooqkhuwaja@gmail.com <fatimafarooqkhuwaja@gmail.com>
2024-03-02 12:03:05 -0800

    Typescript Assignment

---
+++ main.js
@@ -0,0 +1,45 @@
+"use strict";
+//camelCase
+let cityName = "Karachi";
+console.log(cityName);
+//snack_case
+let fav_food = "biryani";
+console.log(fav_food);
+//PascalCase
+let MyPen = "black";
+console.log(MyPen);
+//Cheak Data Type
+//string
+let country = "Pakistan";
+console.log(typeof (country));
+//number
+let myNum = 11;
+console.log(typeof (myNum));
+//boolean
+let password = "abc123";
+console.log(typeof (password == "abc123"));
+console.log(password == "abc123");
+console.log(password != "abc123");
+//Any
+let my_name = "fatima";
+let my_num = 12345;
+let concat = my_name + my_num;
+console.log(concat);
+console.log(typeof (my_name));
+console.log(typeof (my_num));
+console.log(typeof (concat == "fatima12345"));
+//Concatination
+let numb = 5 + 4;
+console.log(numb);
+let numb1 = "5+4";
+console.log(numb1);
+let num_1 = 12;
+let num_2 = 11;
+console.log(num_1 + num_2);
+console.log("num_1 = " + num_1);
+console.log("num_2 = " + num_2);
+console.log("num_1 + num_2 = ", num_1 + num_2);
+let count = 6 > 3;
+console.log(count);
+let count_1 = 5 > 7;
+console.log(count_1);
---
+++ main.ts
@@ -0,0 +1,60 @@
+//camelCase
+let cityName:string = "Karachi"
+console.log(cityName)
+
+//snack_case
+let fav_food:string = "biryani"
+console.log(fav_food)
+
+//PascalCase
+let MyPen:string = "black"
+console.log(MyPen)
+
+//Cheak Data Type
+//string
+let country = "Pakistan"
+console.log(typeof(country))
+
+//number
+let myNum = 11
+console.log(typeof(myNum))
+
+//boolean
+let password:string = "abc123"
+console.log(typeof(password == "abc123"))
+console.log(password == "abc123")
+console.log(password != "abc123")
+ 
+//Any
+let my_name = "fatima"
+let my_num = 12345
+let concat = my_name + my_num
+console.log(concat)
+console.log(typeof(my_name))
+console.log(typeof(my_num))
+console.log(typeof(concat == "fatima12345"))
+
+//Concatination
+let numb = 5+4
+console.log(numb)
+
+let numb1 ="5+4"
+console.log(numb1)
+
+let num_1:number = 12
+let num_2:number = 11
+console.log(num_1 + num_2)
+console.log("num_1 = "+ num_1)
+console.log("num_2 = "+ num_2)
+console.log("num_1 + num_2 = " ,num_1 + num_2)
+
+let count = 6 > 3
+console.log(count)
+
+let count_1 = 5 > 7
+console.log(count_1) 
+
+
+
+
+
